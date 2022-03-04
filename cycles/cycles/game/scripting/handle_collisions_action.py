3c8dad91e197bd3ed1410c2284704b2ead25780d
jessicabreedlove <jessicabreedlove@gmail.com>
2022-03-03 17:10:00 -0700

    followed video

--- cycles/cycles/game/scripting/handle_collisions_action.py
+++ cycles/cycles/game/scripting/handle_collisions_action.py
@@ -36,51 +36,52 @@ class HandleCollisionsAction(Action):
         Args:
             cast (Cast): The cast of Actors in the game.
         """
-        score = cast.get_first_actor("scores")
-        food = cast.get_first_actor("foods")
-        snake = cast.get_first_actor("snakes")
-        head = snake.get_head()
+        # score = cast.get_first_actor("scores")
+        # food = cast.get_first_actor("foods")
+        # snake = cast.get_first_actor("snakes")
+        # head = snake.get_head()
 
-        if head.get_position().equals(food.get_position()):
-            points = food.get_points()
-            snake.grow_tail(points)
-            score.add_points(points)
-            food.reset()
-    
+        # if head.get_position().equals(food.get_position()):
+        #     points = food.get_points()
+        #     snake.grow_tail(points)
+        #     score.add_points(points)
+        #     food.reset()
+        pass
     def _handle_segment_collision(self, cast):
         """Sets the game over flag if the snake collides with one of its segments.
         
         Args:
             cast (Cast): The cast of Actors in the game.
         """
-        snake = cast.get_first_actor("snakes")
-        head = snake.get_segments()[0]
-        segments = snake.get_segments()[1:]
+        # snake = cast.get_first_actor("snakes")
+        # head = snake.get_segments()[0]
+        # segments = snake.get_segments()[1:]
         
-        for segment in segments:
-            if head.get_position().equals(segment.get_position()):
-                self._is_game_over = True
-        
+        # for segment in segments:
+        #     if head.get_position().equals(segment.get_position()):
+        #         self._is_game_over = True
+        pass
     def _handle_game_over(self, cast):
         """Shows the 'game over' message and turns the snake and food white if the game is over.
         
         Args:
             cast (Cast): The cast of Actors in the game.
         """
-        if self._is_game_over:
-            snake = cast.get_first_actor("snakes")
-            segments = snake.get_segments()
-            food = cast.get_first_actor("foods")
+        # if self._is_game_over:
+        #     snake = cast.get_first_actor("snakes")
+        #     segments = snake.get_segments()
+        #     food = cast.get_first_actor("foods")
 
-            x = int(constants.MAX_X / 2)
-            y = int(constants.MAX_Y / 2)
-            position = Point(x, y)
+        #     x = int(constants.MAX_X / 2)
+        #     y = int(constants.MAX_Y / 2)
+        #     position = Point(x, y)
 
-            message = Actor()
-            message.set_text("Game Over!")
-            message.set_position(position)
-            cast.add_actor("messages", message)
+        #     message = Actor()
+        #     message.set_text("Game Over!")
+        #     message.set_position(position)
+        #     cast.add_actor("messages", message)
 
-            for segment in segments:
-                segment.set_color(constants.WHITE)
-            food.set_color(constants.WHITE)
+        #     for segment in segments:
+        #         segment.set_color(constants.WHITE)
+        #     food.set_color(constants.WHITE)
+        pass
