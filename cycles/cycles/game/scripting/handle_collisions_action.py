d18326f2117bccd610fd1e508181b7dd7fce40f9
jessicabreedlove <jessicabreedlove@gmail.com>
2022-03-08 22:04:30 -0700

    error

--- cycles/cycles/game/scripting/handle_collisions_action.py
+++ cycles/cycles/game/scripting/handle_collisions_action.py
@@ -108,12 +108,11 @@ class HandleCollisionsAction(Action):
 
              for segment in segments:
                  segment.set_color(constants.WHITE)
- main
+
     
     def get_is_game_over(self):
         return self._is_game_over
-
-             for segment in segments2:
-                 segment.set_color(constants.WHITE)
+        for segment in segments2:
+            segment.set_color(constants.WHITE)
         
- main
+
